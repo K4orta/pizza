505dd6b602caa3d975643be1a27b14c9a16cf9e6
Erik Wong <ewong@blizzard.com>
2015-08-05 20:43:59 -0700

    Add some basic types

--- pizza.go
+++ pizza.go
@@ -1,1 +1,16 @@
 package pizza
+
+// StreetAddress represents
+type StreetAddress struct {
+	Street  string
+	Street2 string
+	City    string
+	State   string
+	Zip     string
+}
+
+// Store represents
+type Store struct {
+	ID      string
+	Address StreetAddress
+}
